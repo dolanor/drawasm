fee3d62421526562f79d1082a915910167ac3f8f
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2019-07-26 17:38:38 +0200

    Add some info for go generate

--- cmd/drawasm/wasm_exec_tinygo.go
+++ cmd/drawasm/wasm_exec_tinygo.go
@@ -1,6 +1,8 @@
 // +build tinygo
+
 package main
 
+//go:generate echo Generating draw.wasm with TinyGo and copy the correct version of wasm_exec.js
 //go:generate mkdir -p ../../static/wasm
 //go:generate tinygo build -o ../../static/wasm/draw.wasm ../drawasm/
 //go:generate cp wasm_exec_tinygo.js ../../static/js/wasm_exec.js
