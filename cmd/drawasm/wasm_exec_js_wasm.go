426ea4aba3f411b122a9856e2a43114251ad1ae6
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2019-07-26 17:37:45 +0200

    Harmonize file name

--- cmd/drawasm/wasm_exec_js_wasm.go
+++ cmd/drawasm/wasm_exec_js_wasm.go
@@ -3,4 +3,4 @@ package main
 // Since the filename is explicitely *_js_wasm.go, we don't have to use // +build js,wasm build tag.
 //go:generate mkdir -p ../../static/wasm
 //go:generate go build -o ../../static/wasm/draw.wasm ../drawasm/
-//go:generate cp wasm_exec.js ../../static/js/wasm_exec.js
+//go:generate cp wasm_exec_js_wasm.js ../../static/js/wasm_exec.js
--- cmd/drawasm/wasm_exec_tinygo.go
+++ cmd/drawasm/wasm_exec_tinygo.go
@@ -3,4 +3,4 @@ package main
 
 //go:generate mkdir -p ../../static/wasm
 //go:generate tinygo build -o ../../static/wasm/draw.wasm ../drawasm/
-//go:generate cp wasm_exec.tinygo.js ../../static/js/wasm_exec.js
+//go:generate cp wasm_exec_tinygo.js ../../static/js/wasm_exec.js
